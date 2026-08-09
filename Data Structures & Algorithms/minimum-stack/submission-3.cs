b980eb5a6542d18b6f1649e5de57da98bd28f1ae
saloni1031 <138013332+saloni1031@users.noreply.github.com>
2026-08-09 16:14:16 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cs
@@ -0,0 +1,33 @@
+public class MinStack {
+
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+        if(minStack.Count == 0){
+            minStack.Push(val);
+        }
+        else{
+            minStack.Push(Math.Min(val, minStack.Peek()));
+        }
+    }
+    
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
